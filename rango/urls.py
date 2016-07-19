6aca4e0064240858db5cc1e260cce3f2ff2f9945
jtr109 <lyp_login@outlook.com>
2016-07-19 18:12:06 +0800

    chapter 9.5

--- rango/urls.py
+++ rango/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
 from rango import views
 
@@ -8,4 +9,13 @@ urlpatterns = patterns(
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+    )
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,8 @@ TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
