fb286fd890938b634b325cc136301899339de1dc
jtr109 <lyp_login@outlook.com>
2016-07-21 15:28:28 +0800

    Chapter 15: Outer Search API

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 
 def index(request):
@@ -229,3 +230,18 @@ def restricted(request):
 #
 #     # Take the user back to the homepage.
 #     return HttpResponseRedirect('/rango/')
+
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
+
